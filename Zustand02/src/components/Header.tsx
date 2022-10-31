5ce3a985e57ead56b9e96378d09319abd15013a2
yamada <shinya.yamada@benext-corp.com>
2022-11-01 07:06:25 +0900

    ⚡️ Zustand 2 : how to use middleware

---
+++ Zustand02/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { FC, useContext } from "react";
+import { TodoCounter } from "./TodoCounter";
+
+export const Header:FC = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <a>React状態管理</a>
+        </Link>
+        <Link href="/">
+          <a>TODO一覧</a>
+        </Link>
+        <Link href="/add">
+          <a>TODO追加</a>
+        </Link>
+      </nav>
+
+      <TodoCounter />
+    </header>
+  )
+}
